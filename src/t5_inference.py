7ab2f0373b889502881dafe278fe1d22a9071886
Praveen Hegde <connectpraveenhegde@gmail.com>
2024-04-20 14:50:05 +0530

    inference from hf saved model

--- src/t5_inference.py
+++ src/t5_inference.py
@@ -1,19 +1,17 @@
 import torch
 from transformers import T5ForConditionalGeneration, T5Tokenizer
 
-MODEL_ID = "/home/ubuntu/QueryReformulationRL/models/QueryReformulationRL"
+MODEL_ID = "prhegde/t5-query-reformulation-RL"
 
 tokenizer = T5Tokenizer.from_pretrained(MODEL_ID)
-input_sequence = "how to bake great cookie"
-input_ids = tokenizer(input_sequence, return_tensors="pt").input_ids
-nsent = 4
-
-print(f'Input: {input_sequence}')
-
 model = T5ForConditionalGeneration.from_pretrained(MODEL_ID)
-
 model.eval()
 
+input_sequence = "how to bake great cookie"
+input_ids = tokenizer(input_sequence, return_tensors="pt").input_ids
+print(f'Input: {input_sequence}')
+
+nsent = 4
 with torch.no_grad():
     for i in range(nsent):
         output = model.generate(input_ids, max_length=35, num_beams=1, do_sample=True, repetition_penalty=1.8)
